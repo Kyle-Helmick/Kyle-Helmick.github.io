01542d5c6e8f34fc8958eea24361363938587d21
Kyle-Helmick <helmick.kyle@gmail.com>
2019-07-29 21:04:12 -0700

    ⚗ Cleaner and CNAME test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import CenterPage from "./Components/CenterPage";
+import { Alert } from "reactstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <CenterPage>
-        <h1>
-          <span role="img" aria-label="Construction Sign">
-            🚧{" "}
-          </span>
-          Looks like we're under construction
-          <span role="img" aria-label="Construction Sign">
-            {" "}
-            🚧
-          </span>
-        </h1>
+        <Alert color="secondary">
+          <h2>
+            <span role="img" aria-label="Construction Sign">
+              🚧{" "}
+            </span>
+            Looks like we're under construction, come back later!
+            <span role="img" aria-label="Construction Sign">
+              {" "}
+              🚧
+            </span>
+          </h2>
+        </Alert>
       </CenterPage>
     </div>
   );
